381a85c6ab853b1da521d6143d19e43300d27fba
Rodrigo <rodrigoantonioroaroa@gmail.com>
2026-06-17 10:50:25 -0400

    ejercicios 17/06

---
+++ Ejercicio-4.py
@@ -0,0 +1,12 @@
+peliculas = ["Inception", "Marix", "Interstellar"]
+
+peliculas.append("Avatar")
+
+primera_pelicula = peliculas[0] 
+
+pelicula = {
+    "nombre": "inception",
+    "año": 2010,
+    "categoria": "Ciencia Ficcion"
+}
+nombre = pelicula["nombre"]
